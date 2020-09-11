12ac245317811c7ec3859ae5e58d8ebb9a6a4708
unknown <fa17-bce-062@cuilahore.edu.pk>
2020-09-12 01:45:57 +0500

    Assignment 1 complete updated

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,83 +1,61 @@
 import React, { Component } from 'react';
-import { View, FlatList,Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { ScrollView, Text, View, FlatList } from 'react-native';
+import { ListItem, Card } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
-import { Card } from 'react-native-elements';
-import { SectionList, StyleSheet } from 'react-native';
+function History(){
+	return(
+        <View>
+            <Card
+                title="Our History"
+            >
+        
+                <Text style={{margin: 10}}>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
+                <Text style={{margin: 10}}>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.
+               </Text> 
+                   
+            </Card>
+        </View>
+    );
+}
 
-const styles = StyleSheet.create({
-    container: {
-     flex: 1,
-     paddingBottom: 0,
-     paddingTop: 0
-    },
-    sectionHeader: {
-      paddingTop: 0,
-      paddingLeft: 0,
-      paddingRight: 0,
-      paddingBottom: 0,
-      fontSize: 20,
-      fontWeight: 'bold',
-      alignContent: 'center'
-
-      
-    }
-  })
-  
-class About extends Component {
-
-    constructor(props) {
+class About extends Component{
+     constructor(props) {
         super(props);
         this.state = {
             leaders: LEADERS
         };
     }
 
-
-    static navigationOptions = {
-        title: 'About Us',
-    };
-    render(){
-        
-        const renderItem = ({item, index}) => {
-            return (
+    render()
+	{
+		const renderLeaderItem = ({item,index}) => {
+            return(
                 <ListItem
-                key={index}
-                title={item.name}
-                subtitle={item.description}
-                hideChevron={true}
-                leftAvatar={{ source: require('./images/alberto.png')}}
-              >
-              </ListItem>
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    hideChevron={true}
+                    leftAvatar={{ source: require('./images/alberto.png')}}
+                />
             );
-        };
-
-        return (
-            
-            <View style={styles.container}>
-                <Card>
-                   <Text style={styles.sectionHeader}>Our History</Text>
-                   <Text >Started in 2010, Ristorante con Fusion quickly established itself as a culinary 
-                          icon par excellence in Hong Kong. With its unique brand of world fusion cuisine
-                          that can be found nowhere else, it enjoys patronage from the A-list clientele
-                          in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world,
-                          you never know what will arrive on your plate the next time you visit us.</Text>
-
-                    <Text>The restaurant traces its humble beginnings to The Frying Pan, a successful
-                          chain started by our CEO, Mr. Peter Pan, that featured for the first time the 
-                          world's best cuisines in a pan.</Text>
+           }
+           return(
+            <ScrollView>
+                <History/>
+                <Card
+                    title="Corporate Leadership"
+                >
+                    <FlatList
+                        data={this.state.leaders}
+                        renderItem={renderLeaderItem}
+                        keyExtractor={item => item.id.toString()}
+                    />
                 </Card>
-           
-                <Card >  
-                    <Text  style={styles.sectionHeader}>Corparate Leaders</Text>
-                    <FlatList 
-                         data={this.state.leaders}
-                        renderItem={renderItem}
-                        />
-                </Card>
-               
-            </View>
-    );
-    }
+            </ScrollView>
+        );
+	}
+   
 }
+
+
 export default About;
